efa5d9f4593a671db9da2692a66e20e94ebd7c0b
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-02 21:51:53 +0530

    Routing Task

---
+++ 02.routing-task/app/products/[id]/page.js
@@ -0,0 +1,8 @@
+export default function ProductId({params}){
+    const {id} = params;
+    return(
+        <>
+            <h1>Product {id} details page — content coming soon!</h1>
+        </>
+    );
+}
---
+++ 02.routing-task/app/products/page.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Products(){
+    return(
+        <>
+            <h1>Products Page</h1>
+            <hr/>
+            <Link href='/products/1'>Product 1</Link>
+            <hr/>
+            <Link href='/products/2'>Product 2</Link>
+            <hr/>
+            <Link href='/products/3'>Product 3</Link>
+            <hr/>
+            <Link href='/products/4'>Product 4</Link>
+            <hr/>
+            <Link href='/products/5'>Product 5</Link>
+            <hr/>
+            <Link href='/products/6'>Product 6</Link>
+            <hr/>
+            <Link href='/products/7'>Product 7</Link>
+            <hr/>
+            <Link href='/products/8'>Product 8</Link>
+            <hr/>
+            <Link href='/products/9'>Product 9</Link>
+            <hr/>
+            <Link href='/products/10'>Product 10</Link>
+            <hr/>
+        </>
+    );
+}
