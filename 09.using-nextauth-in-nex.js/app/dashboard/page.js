ffab0e005720dd9920ff6e7e75e3f4c06840c2b1
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-04 05:47:26 +0530

    Using NextAuth In NextJS

---
+++ 09.using-nextauth-in-nex.js/app/dashboard/page.js
@@ -0,0 +1,29 @@
+"use client";
+import {useSession, signIn, signOut} from "next-auth/react";
+
+export default function Dashboard() {
+
+    const {data: session, status} = useSession();
+
+    if(status==="loading"){
+        return <p>Loading...</p>;
+    }
+
+    if(session){
+        return(
+            <div>
+                <p>The user is logged in</p>
+                <p>Signed in as : {session.user.name || session.user.email}</p>
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <p>Not Signed in</p>
+            <button onClick={() => signIn("github")}>Sign in</button>
+        </div>
+    )
+}
+
